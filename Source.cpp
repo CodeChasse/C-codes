6f965cc4b7b1817088e4622790ba9e30141d8fae
Mukarrab <133678237+CodeChasse@users.noreply.github.com>
2023-05-22 12:33:05 -0700

    stack library
    using all functions of stack from library. not all but a good practice example

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,28 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 
-
-void display( int n)
-{
-	if (n < 1)
-	{
-		return;
-	}
-	else
-	{
-		cout << n << endl;
-		display(n - 1);
-		cout << n << endl;
-	}
-}
 int main()
 {
+	stack<int>s;
+	for (int i = 0; i < 5; i++)
+	{
+		s.push(i);
+	}
 
-	display(5);
+	cout <<  "size = " << s.size();
+	cout << "top = " << s.top();
+
+	s.pop();
+	cout << "size = " << s.size();
+	s.push(6);
+	s.push(7);
+
+	cout<<"size = "<<s.size();
+
+
+
+
 
 	system("pause");
 	return 0;
