18b9a7f54210abe974656ec83cdf0f1cdb7135ed
Mukarrab <133678237+CodeChasse@users.noreply.github.com>
2023-06-04 11:08:36 -0700

    queue with array

--- Source.cpp
+++ Source.cpp
@@ -1,73 +1,55 @@
 #include <iostream>
-using namespace std;
-class Car
-{
+
+class ArrayQueue {
 private:
-	int speed;
-	char* name;
-	bool isOn;
+	int front;
+	int rear;
+	int capacity;
+	int* queueArray;
+
 public:
-	Car();
-	void setName(char*);
-	char* getName();
+	ArrayQueue(int size) {
+		capacity = size;
+		front = rear = -1;
+		queueArray = new int[capacity];
+	}
 
-	void accelerate();
-	void brake();
-	int getSpeed();
+	~ArrayQueue() {
+		delete[] queueArray;
+	}
 
-	void start();
-	void stop();
-	bool getIgnition();
+	bool isEmptyQueue() {
+		return front == -1;
+	}
 
-	void display();
+	bool isFullQueue() {
+		return (rear + 1) % capacity == front;
+	}
+
+	void enQueue(int data) {
+		if (isFullQueue()) {
+			std::cout << "Queue Overflow!" << std::endl;
+			return;
+		}
+
+		rear = (rear + 1) % capacity;
+		queueArray[rear] = data;
+
+		if (front == -1)
+			front = rear;
+	}
+
+	void deQueue() {
+		if (isEmptyQueue()) {
+			std::cout << "Queue Underflow!" << std::endl;
+			return;
+		}
+
+		if (front == rear) {
+			front = rear = -1;
+		}
+		else {
+			front = (front + 1) % capacity;
+		}
+	}
 };
-Car::Car()
-{
-	cout << "Car Constructor is called" << endl;
-	speed = 0;
-	name = "";
-	isOn = false;
-}
-void Car::setName(char* n)
-{
-	name = n;
-}
-void Car::display()
-{
-	cout << "Car Object" << endl;
-	cout << "Name: " << name << endl;
-	cout << "Speed: " << speed << endl;
-	cout << "isOn: " << isOn << endl;
-}
-void Car::accelerate()
-{
-	if (speed<100)
-	{
-		speed += 5;
-	}
-}
-void Car::brake()
-{
-	if (speed != 0)
-	{
-		speed -= 5;
-	}
-}
-int main()
-{
-	Car c1;
-	c1.display();
-	c1.setName("Honda");
-	for (int i = 1; i < 100; i++)
-	{
-		c1.accelerate();
-	}
-	c1.display();
-	for (int i = 1; i <= 25; i++)
-	{
-		c1.brake();
-	}
-	c1.display();
-
-	return 0;
-}
